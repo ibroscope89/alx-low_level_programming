724532fcf79e37eb96fe19611bac807ed6e62f42
ibroscope89 <ibroscope89@gmail.com>
2022-03-16 02:59:57 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * main - Putchar
- *
- * _putchar print the characters given
- * Return: Always 0 (Success)
- */
+*main - prints the word _putchar
+*
+*Return: 0 on success execution.
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
 	return (0);
 }
